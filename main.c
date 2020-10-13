7ea81450eda8a24df43f960aa8c7e23a3e5d4e9a
vllutan <43119331+vllutan@users.noreply.github.com>
2020-10-13 09:19:33 +0300

    Add files via upload
    Первый практикум

---
+++ main.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int ch;
+
+struct node{
+  char *word;
+  int amount;
+  struct node *left;
+  struct node *right;
+};
+
+int isword(int c){
+  return (isalnum(c) || c=='-' || c=='_') ? 1 : 0;
+}
+
+struct node *AddInFreqTree(struct node *head, char *str, int num, FILE *f_out){
+  struct node *p, *q;
+  p=head; q=NULL;
+
+//  printf("%s\n", str);
+
+  while (p != NULL){
+    q = p;
+    if (num > p->amount) p = p->right;
+    else p = p->left;
+  }
+
+  struct node *new;
+  new = (struct node*)malloc(sizeof(struct node));
+  new->word = malloc(strlen(str));
+  strcpy(new->word, str);
+  new->amount = num;
+  new->left = NULL;
+  new->right = NULL;
+
+  if (q == NULL) {
+    head = new;
+    //printf("%s %d\n", head->word, head->amount);
+  } else if(num > q->amount) {
+    q->right = new;
+    q=q->right;
+    //printf("%s %d\n", q->word, q->amount);
+  } else {
+    q->left = new;
+    q=q->left;
+    //printf("%s %d\n", q->word, q->amount);
+  }
+
+  return head;
+}
+
+struct node *AddInDict(struct node *head, char *str){
+  struct node *p, *q;
+  p=head; q=NULL;
+  int res;
+
+ // printf("%s\n", str);
+
+  while (p != NULL){
+    q = p;
+    res = strcmp(str, p->word);
+    if (res == 0) {
+      p->amount++;
+      //printf("%s %d\n", p->word, p->amount);
+      return head;
+    } else if (res > 0) p = p->right;
+    else p = p->left;
+  }
+
+  struct node *new;
+  new = (struct node*)malloc(sizeof(struct node));
+  new->word = malloc(strlen(str));
+  strcpy(new->word, str);
+  new->amount = 1;
+  new->left = NULL;
+  new->right = NULL;
+
+  if (q == NULL) {
+    head = new;
+    //printf("%s %d\n", head->word, head->amount);
+  } else if(strcmp(str, q->word) > 0) {
+    q->right = new;
+    q=q->right;
+    //printf("%s %d\n", q->word, q->amount);
+  } else {
+    q->left = new;
+    q=q->left;
+    //printf("%s %d\n", q->word, q->amount);
+  }
+  return head;
+}
+
+void Obhod(struct node *head, FILE *f_out, int total){
+  if (head != NULL){
+    Obhod(head->right, f_out, total);
+    double freq=(head->amount)/(total*1.0);
+    fprintf(f_out, "%s %d %f\n", head->word, head->amount, freq);
+    Obhod(head->left, f_out, total);
+  }
+}
+
+struct node *Obhod2(struct node *d_head, struct node *q_tree_head, FILE *f_out){
+  if (d_head != NULL){
+    q_tree_head = Obhod2(d_head->left, q_tree_head, f_out);
+    q_tree_head = AddInFreqTree(q_tree_head, d_head->word, d_head->amount, f_out);
+    q_tree_head = Obhod2(d_head->right, q_tree_head, f_out);
+  }
+  return q_tree_head;
+}
+
+void DeleteTree(struct node *head){
+  if (head != NULL){
+    DeleteTree(head->left);
+    DeleteTree(head->right);
+    free(head);
+  }
+}
+
+char *NewWord(FILE *f_inp){
+  char *str=NULL;
+  int i=0, str_size=0;
+  while( isspace(ch) ) ch=getc(f_inp);
+  //printf("bp2 %c", ch);
+  if ( isword(ch) ) {
+    //printf("bp3 ");
+    while( isword(ch) ) {
+      if (i == str_size) {
+        str_size = 2 * str_size + 1;
+        str = (char *) realloc(str, str_size);
+        if (str == NULL) printf("str realloc error\n");
+      }
+      str[i] = ch;
+      i++;
+      ch = getc(f_inp);
+    }
+    str=realloc(str, str_size + 1);
+    str[i]='\0';
+  } else if ((ch != EOF) ){//&& ispunct(ch)){
+    //printf("bp4\n");
+    str = malloc(2* sizeof(int));
+    str[0] = ch;
+    str[1] = '\0';
+    ch = getc(f_inp);
+  }
+  //printf("%s %c %d\n", str, ch, ch);
+  return str;
+}
+
+int main(int argc, char *argv[]) {
+  FILE *f_inp=stdin;
+  //fopen(argv[argc - 2], "r");
+  FILE *f_out=stdout;
+  //fopen(argv[argc - 1], "w");
+
+  if (argc >= 2) {
+    if (!strcmp(argv[1], "-i")) f_inp=fopen(argv[2], "r");
+    else if (!strcmp(argv[1], "-o")) f_out=fopen(argv[2], "w");
+    if (argc >= 4) {
+      if (!strcmp(argv[3], "-i")) f_inp=fopen(argv[4], "r");
+      else if (!strcmp(argv[3], "-o")) f_out=fopen(argv[4], "w");
+    }
+  }
+
+  if (f_inp == NULL) fprintf(stderr, "f_inp opened incorrectly"); else printf("f1 ok\n");
+  if (f_inp == stdin) printf("f1 stdin");
+  if (f_out == NULL) fprintf(stderr, "f_out opened incorrectly"); else printf("f2 ok\n");
+  if (f_out == stdout) printf("f2 stdout");
+
+  struct node *dict_head=NULL;
+  char *str=NULL;
+  int total=0;
+ /* int s = 0;
+
+  while ((c = getc(f_inp)) != EOF) {
+    if ((c > 0) && (c < 255)) {
+      printf("%c", c);
+    } else printf("//");
+    if (c=='a') s++;
+  }
+
+  printf("%d\n", s);
+  rewind(f_inp);
+  printf("%d\n", isgraph(EOF));*/
+
+  ch = getc(f_inp);
+  while (ch != EOF){
+    str = NewWord(f_inp);
+    if (str != NULL){
+      total++;
+      dict_head = AddInDict(dict_head, str);
+      free(str);
+    }
+  }
+
+  printf("Dictionary is successfully built, total:  %d\n", total);
+
+  Obhod(dict_head, f_out, total);
+  fprintf(f_out, "\n\n-------\n\n");
+
+  struct node *quant_tree_head=NULL;
+
+  quant_tree_head = Obhod2(dict_head, quant_tree_head, f_out);
+
+//  printf("\n%d %d\n\n\n\n", (dict_head == NULL), (quant_tree_head == NULL));
+
+  Obhod(quant_tree_head, f_out, total);
+
+
+  DeleteTree(dict_head);
+  DeleteTree(quant_tree_head);
+
+
+  if (f_inp!=stdin) fclose(f_inp);
+  if (f_out!=stdout) fclose(f_out);
+
+  return 0;
+}

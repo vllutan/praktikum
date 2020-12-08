b0e5f2b0f0a2d0f9c67911fe36674358315fff0b
vllutan <43119331+vllutan@users.noreply.github.com>
2020-12-08 16:27:35 +0300

    final

--- task1.c
+++ task1.c
@@ -39,29 +39,26 @@ int main(int argc, char **argv) {
     printf("%d\n", ret);
     perror("execlp 1 error: ");
 
-  } else {
-    pid = waitpid(id, &status, 0);
+  } else wait(&status);
 
-    printf("%d\n", WIFEXITED(status));
-    if (WIFEXITED(status) == 0){
-      pid = fork();
-      if (pid < 0) {
-        perror("fork error: ");
-        exit(1);
-      } else if (pid == 0) {
-        dup2(fd[1], 1);
-        close(fd[0]);
-        close(fd[1]);
+  if (status != 0) {
+    pid = fork();
+    if (pid < 0) {
+      perror("fork error: ");
+      exit(1);
+    } else if (pid == 0) {
+      dup2(fd[1], 1);
+      close(fd[0]);
+      close(fd[1]);
 
-        execlp(argv[2], argv[2], NULL);
-        perror("execlp 2 error: ");
-        //exit(3);
-      } else {
-        wait(NULL);
-      }
+      execlp(argv[2], argv[2], NULL);
+      perror("execlp 2 error: ");
+      //exit(3);
+    } else {
+      wait(NULL);
     }
   }
-
+  
   dup2(fd[0], 0);
   close(fd[0]);
   close(fd[1]);

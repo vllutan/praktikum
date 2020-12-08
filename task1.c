b7e64eb5fa19ca142281549b20679803b7b37943
vllutan <43119331+vllutan@users.noreply.github.com>
2020-12-08 13:15:33 +0300

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int main() {
+
+  return 0;
+}

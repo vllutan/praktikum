823c30a6e2980eed8f8ea376fadfa2ffaf94a8d9
vllutan <43119331+vllutan@users.noreply.github.com>
2021-05-04 16:13:51 +0300

    Add files via upload

--- socket.cpp
+++ socket.cpp
@@ -10,10 +10,18 @@
 #include <ctime>
 #include <fstream>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <cerrno>
 
 using namespace std;
 
-const int BACKLOG = 5;
+#define BACKLOG 5
+#define SERVER_ADDR "127.0.0.1"
+#define cgi_SERVER_ADDR "SERVER_ADDR=127.0.0.1"
+#define SERVER_PORT 1234
+#define cgi_SERVER_PORT "SERVER_PORT=1234"
+#define SERVER_SOFTWARE "Apache/2.4.41 (Ubuntu)"
+#define cgi_SERVER_SOFTWARE "SERVER_SOFTWARE=Apache/2.4.41 (Ubuntu)"
 
 class SocketAddress {     // IP and port number
   struct sockaddr_in saddr;
@@ -155,7 +163,7 @@ class HttpHeader{
 
   string to_string(){
     string res;
-    for (int i=0; i < lines.size(); ++i){
+    for (size_t i=0; i < lines.size(); ++i){
       res += lines[i];
     }
     return res;
@@ -166,7 +174,7 @@ class HttpResponse{
   string start_header;
   int code;
   string method;
-  string headers;
+  std::vector<HttpHeader> headers;
   std::vector<uint8_t> body;
 
  public:
@@ -177,7 +185,7 @@ class HttpResponse{
 
  string get_start_header() const {return start_header;}
  int get_code() const {return code;}
- string get_headers() const {return headers;}
+ //string get_headers() const {return headers;}
  std::vector<uint8_t> get_body() const {return body;}
 
 };*/
@@ -210,6 +218,139 @@ string parse_path(string &line){
   return path;
 }
 
+string get_Date(){
+  time_t rawtime;
+  struct tm *timeinfo;
+  char buffer[40];
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(buffer, 40, "%a, %e %h %G %T GMT", timeinfo);
+  return (string)buffer;
+}
+
+string get_Last_Modified(string &path){
+  struct stat buff;
+  stat(path.c_str(), &buff);
+  struct tm *timeinfo;
+  char buffer[40];
+  time(&buff.st_mtime);
+  timeinfo = localtime(&buff.st_mtime);
+  strftime(buffer, 40, "%a, %e %h %G %T GMT", timeinfo);
+  return (string)buffer;
+}
+
+string get_Content_Type(string &path){
+  string content_type = "";
+  size_t dot = path.rfind('.', path.length());
+  string doc_type = path.substr(dot + 1, path.length() - 1);
+  if ((doc_type == "jpg") || (doc_type == "png") || (doc_type == "jpeg")) {
+    content_type = "image/jpeg";
+  } else if (doc_type == "html") {
+    content_type = "text/html";
+  } else content_type = "text/plain";
+  return content_type;
+}
+
+string get_Query_String(string &path){
+  string res="";
+  size_t quest_mark = path.find('?', 0);
+  if((signed)quest_mark == -1) throw "no ? in cgi path";
+  for(size_t i=quest_mark+1; i<path.length(); ++i) res += path[i];
+  return res;
+}
+
+string get_filename(string &path){
+  string res;
+  for(size_t i=0; path[i] != '?'; ++i) res += path[i];
+  return res;
+}
+
+void cgi_connection(string &path, string &request, ConnectedSocket cs){
+  int fd;
+  int pid = fork();
+  if (pid < 0) {
+    perror("fork error");
+    exit(1);
+  } else if (pid == 0) {
+    std::vector<string> env;
+    string script_filename = get_filename(path);
+
+    env.push_back("CONTENT_TYPE=" + get_Content_Type(path));
+    //env.push_back("GATEWAY_INTERFACE=CGI/1.1");
+    //env.push_back("REMOTE_ADDR=127.0.0.5");
+    //env.push_back("REMOTE_PORT=8845");
+    env.push_back("QUERY_STRING=" + get_Query_String(path));
+    env.push_back(cgi_SERVER_ADDR);
+    env.push_back(cgi_SERVER_PORT);
+    env.push_back("SEVER_PROTOCOL=HTTP/1.1");
+    env.push_back(cgi_SERVER_SOFTWARE);
+    env.push_back("SCRIPT_FILENAME=" + script_filename);
+
+    fd = open("temp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0) throw "temporary cgi file not opening";
+    dup2(fd, 1);
+    close(fd);
+
+    char *argv[] = {(char *) script_filename.c_str(), NULL};
+    char **envir = new char *[env.size() + 1];
+    for (size_t i = 0; i < env.size(); ++i) envir[i] = (char *) env[i].c_str();
+    envir[env.size()] = nullptr;
+
+    execvpe(script_filename.c_str(), argv, envir);
+
+    perror("execvpe error");
+    exit(5);
+  } else {
+    int status; wait(&status);
+
+    if((WIFEXITED(status) != 0) && (WEXITSTATUS(status) == 0)){
+      string header;
+      string method;
+      for(int i=0; request[i] != ' '; ++i) method += request[i];
+      int fd1 = open("temp", O_RDONLY);
+      cs.write__("HTTP/1.1 200 Ok");
+      std::vector<uint8_t> body;
+      char c;
+      while (read(fd1, &c, 1)) body.push_back(c);
+
+      header = "Version: HTTP/1.1\r\nDate: " + get_Date() + "\r\nServer: " + SERVER_SOFTWARE + "\r\n"
+              + "Last-Modified: " + get_Last_Modified(path);
+
+      if (method == "GET") {
+        header += "\r\nContent-Type: " + get_Content_Type(path) + ";charset=utf-8\r\nContent-Length: " + to_string(body.size()) +
+                "\r\n\r\n";
+
+        cs.write__(header);
+        cs.write__(body);
+      } else if (method == "HEAD") {
+        header += "\r\n\r\n";
+
+        cs.write__(header);
+      } else throw "code error";
+
+      cout << header << endl;
+
+      close(fd1);
+    } else {
+      cs.write__("HTTP/1.1 404 Not Found");
+      int fd2 = open("404.html", O_RDONLY);
+      if (fd2 < 0) throw "no 404 error file";
+
+      std::vector<uint8_t> body;
+      char c;
+      while (read(fd2, &c, 1)) body.push_back(c);
+
+      string header = "Version: HTTP/1.1\r\nDate: " + get_Date() + "\r\nServer: " + SERVER_SOFTWARE + "\r\n" +
+          + "Content-Type: text/html;charset=utf-8\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n";
+      cs.write__(header);
+      cs.write__(body);
+
+      cout << header << endl;
+
+      close(fd2);
+    }
+  }
+}
 
 void ProcessConnection(int cd, const SocketAddress &clAddr){
   ConnectedSocket cs(cd);
@@ -225,69 +366,54 @@ void ProcessConnection(int cd, const SocketAddress &clAddr){
   for(int i=0; lines[0][i] != ' '; ++i) method += lines[0][i];
   string path = parse_path(lines[0]);
 
-  int fd = open(path.c_str(), O_RDONLY);                         //default_connection (later with cgi choice)
-  if(fd < 0) {
-    response = "HTTP/1.1 404 Not Found"; code = 404;
-    fd = open("404.html", O_RDONLY);
-    if (fd < 0) throw "no 404 error file";
-  }
-  else {response = "HTTP/1.1 200 Ok"; code = 200; }
-  cs.write__(response);
 
-  std::vector<uint8_t> body;
-  char c;
-  while( read(fd, &c, 1) ) body.push_back(c);             // file to vector
+  if((signed)lines[0].find('?', 0) != -1){
+    cgi_connection(path, lines[0], cs);
+  } else {
+    int fd = open(path.c_str(), O_RDONLY);                         //default_connection
+    if (fd < 0) {
+      response = "HTTP/1.1 404 Not Found";
+      code = 404;
+      fd = open("404.html", O_RDONLY);
+      if (fd < 0) throw "no 404 error file";
+    } else {
+      response = "HTTP/1.1 200 Ok";
+      code = 200;
+    }
+    cs.write__(response);
 
-  time_t rawtime;                                         // get Date
-  struct tm * timeinfo;
-  char buffer[40];
-  time (&rawtime);
-  timeinfo = localtime (&rawtime);
-  strftime (buffer,40,"%a, %e %h %G %T GMT",timeinfo);    //  end get Date
+    std::vector<uint8_t> body;
+    char c;
+    while (read(fd, &c, 1)) body.push_back(c);
 
-  header = "Version: HTTP/1.1\r\nDate: " + (string)buffer + "\r\nServer: Apache/2.4.41 (Ubuntu)\r\n";
+    header = "Version: HTTP/1.1\r\nDate: " + get_Date() + "\r\nServer: " + SERVER_SOFTWARE + "\r\n";
 
-  string content_type="";                                 // get Content-Type
-  int dot = path.rfind('.', path.length());
-  string doc_type = path.substr(dot + 1, path.length()-1);
-  if ((doc_type == "jpg") || (doc_type == "png") || (doc_type == "jpeg")){
-    content_type = "image/jpeg";
-  } else if (doc_type == "html"){
-    content_type = "text/html";
-  } else content_type = "text/plain";
+    string content_type = get_Content_Type(path);
 
+    if (code == 200) {
+      header += "Last-Modified: " + get_Last_Modified(path);
 
-  if(code == 200) {
-    struct stat buff;                                        // get Last-Modified
-    stat(path.c_str(), &buff);
-    struct tm * timeinfo1;
-    char buffer1[40];
-    time (&buff.st_mtime);
-    timeinfo1 = localtime (&buff.st_mtime);
-    strftime (buffer1,40,"%a, %e %h %G %T GMT",timeinfo1);   // end get Last-Modified
+      if (method == "GET") {
+        header += "\r\nContent-Type: " + content_type + ";charset=utf-8\r\nContent-Length: " + to_string(body.size()) +
+                  "\r\n\r\n";
 
-    header += "Last-Modified: " + (string)buffer1;
+        cs.write__(header);
+        cs.write__(body);
+      } else if (method == "HEAD") {
+        header += "\r\n\r\n";
 
-    if(method == "GET"){
-      header += "\r\nContent-Type: " + content_type + ";charset=utf-8\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n";
+        cs.write__(header);
+      } else throw "code error";
+    } else if (code == 404) {
+      header += "Content-Type: text/html;charset=utf-8\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n";
 
       cs.write__(header);
       cs.write__(body);
-    } else if (method == "HEAD") {
-      header += "\r\n\r\n";
+    }
+    cout << header << endl;
 
-      cs.write__(header);
-    } else throw "code error";
-  } else if (code == 404) {
-    header += "Content-Type: " + content_type + ";charset=utf-8\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n";
-
-    cs.write__(header);
-    cs.write__(body);
+    close(fd);
   }
-
-  cout << header << endl;
-
-  close(fd);
   cs.shut_down();
 }
 
@@ -297,7 +423,7 @@ void ServerLoop(){                                                  //server_mai
     if (fd_log < 0) throw "cannot open log";
     close(fd_log);
 
-    SocketAddress serAddr("127.0.0.1", 1234);
+    SocketAddress serAddr(SERVER_ADDR, SERVER_PORT);
     ServerSocket ss;
     ss.bind__(serAddr);
     ss.listen__(BACKLOG);
